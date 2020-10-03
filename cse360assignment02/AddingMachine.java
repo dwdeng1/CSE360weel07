964f313fba6c93b0fe435ce0fb6373e7a1d6919c
dwdeng <derek.deng64@gmail.com>
2020-10-03 16:06:43 -0700

    Added javadoc comments

--- cse360assignment02/AddingMachine.java
+++ cse360assignment02/AddingMachine.java
@@ -1,25 +1,47 @@
+/**
+* This is a package called cse360assignment02
+ *  It contains AddingMachine.java
+*/
 package cse360assignment02;
 public class AddingMachine {
     private int total;
-
+    /**
+     * AddingMachine initializes total
+     *  It is a public method, using total.
+     */
     public AddingMachine () {
         total = 0;  // not needed - included for clarity
     }
-
+    /**
+     * getTotal is a method that returns total's current value.
+     *  It is a public method, using total.
+     */
     public int getTotal () {
         return 0;
     }
-
+    /**
+     * add should add a parameter to total.
+     *  It is a public method, using total and a parameter.
+     */
     public void add (int value) {
     }
-
+    /**
+     * subtract should subtract the parameter from total.
+     *  It is a public method, using total and a parameter.
+     */
     public void subtract (int value) {
     }
-
+    /**
+     * toString should display the keep a history of methods, and print them
+     *  It is a public method and would likely need to somehow record how total changes.
+     */
     public String toString () {
         return "";
     }
-
+    /**
+     * clear should delete the messages from toString
+     *  It is a public method and would likely need to somehow record how total changes.
+     */
     public void clear() {
     }
 }
